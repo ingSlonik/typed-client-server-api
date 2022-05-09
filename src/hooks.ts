54670391d75f75747d184c42cb2dcc80f0b2a796
Filip Paulů <ing.fenix@seznam.cz>
2022-05-09 22:54:23 +0200

    Update hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -7,7 +7,7 @@ export type UseAPIFrontend<T extends API> = {
     [I in keyof T]: (params: T[I]["params"]) => UseResult<T[I]["result"]>;
 };
 
-export { setServerUrl } from "./client";
+export { getAPIFrontend, setServerUrl, setHeaders } from "./client";
 
 export function getUseAPIFrontend<T extends API>(): UseAPIFrontend<T> {
     const api = getAPIFrontend<T>();
@@ -16,12 +16,12 @@ export function getUseAPIFrontend<T extends API>(): UseAPIFrontend<T> {
         get: (_, endpoint: string) => {
 
             return (params: any) => {
-                const [ result, setResult ] = useState<UseResult<any>>([ null, null, true, 0 ]);
+                const [result, setResult] = useState<UseResult<any>>([null, null, true, 0]);
 
                 useEffect(() => {
-                    api[endpoint](params).then((result: any) => setResult([ result[0], result[1], false, result[2] ]));
-                // eslint-disable-next-line react-hooks/exhaustive-deps
-                }, [ JSON.stringify(params) ]);
+                    api[endpoint](params).then((result: any) => setResult([result[0], result[1], false, result[2]]));
+                    // eslint-disable-next-line react-hooks/exhaustive-deps
+                }, [JSON.stringify(params)]);
 
                 return result;
             };
