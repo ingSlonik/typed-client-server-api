0c02d9a4f508c200f675078b7e9705773a24e187
Filip Paulů <ing.fenix@seznam.cz>
2022-05-11 18:17:53 +0200

    Fix export variables for hooks

--- hooks.js
+++ hooks.js
@@ -1,4 +1,6 @@
 var hooks = require("./client/hooks");
 exports.default = hooks;
+exports.setHeaders = hooks.setHeaders;
 exports.setServerUrl = hooks.setServerUrl;
+exports.getAPIFrontend = hooks.getAPIFrontend;
 exports.getUseAPIFrontend = hooks.getUseAPIFrontend;
