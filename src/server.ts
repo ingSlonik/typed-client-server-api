f331ef9e600608e51f7aa3c14396f74c8286c4bc
Filip Paulů <ing.fenix@seznam.cz>
2025-02-25 00:39:34 +0100

    Implement API Error

--- src/server.ts
+++ src/server.ts
@@ -2,34 +2,23 @@ import { Express, Request, Response } from "express";
 
 import { API, getMethodFromEndpoint, getUrlFromEndpoint } from "./index";
 
+type MethodLowerCase = "get" | "post" | "put" | "delete";
+
 export type APIBackendImplementation<T extends API> = {
     [I in keyof T]: (params: T[I]["params"], req: Request, res: Response) => Promise<T[I]["result"]>;
 };
 
 export function setAPIBackend<T extends API>(app: Express, api: APIBackendImplementation<T>): void {
     for (const endpoint in api) {
-        const method = getMethodFromEndpoint(endpoint).toLowerCase() as "get" | "post" | "put" | "delete";
+        const method = getMethodFromEndpoint(endpoint).toLowerCase() as MethodLowerCase;
         const url = getUrlFromEndpoint(endpoint);
 
         app[method](url, async (req, res) => {
-            let params: any = {};
-            if (method === "get" || method === "delete") {
-                for (let [key, value] of Object.entries(req.query)) {
-                    if (typeof value === "string") {
-                        try {
-                            value = JSON.parse(value);
-                        } catch (e) { }
-                    }
-                    params[key] = value;
-                }
-            } else {
-                params = req.body;
-            }
+            const params = getParams(method, req);
 
             const log = { date: new Date().toISOString(), url, method, params };
 
             if (process.env.NODE_ENV === "development")
-                // eslint-disable-next-line no-console
                 console.log(`${log.date}: ${method} ${url}`, params);
 
             try {
@@ -38,12 +27,40 @@ export function setAPIBackend<T extends API>(app: Express, api: APIBackendImplem
                 res.setHeader("Cache-Control", "no-cache");
                 res.json(result || null);
             } catch (e: any) {
-                const message = typeof e === "object" && e !== null && typeof e.message === "string" ? e.message : JSON.stringify(e);
-                // eslint-disable-next-line no-console
-                console.log("Error:", new Date(), url, message);
-                res.status(400);
+                const message = e && typeof e === "object" && typeof e.message === "string" ? e.message : JSON.stringify(e);
+                const status = e && typeof e === "object" && typeof e.status === "number" ? e.status : 400;
+
+                console.log("API Error:", new Date(), url, status, message);
+
+                res.status(status);
                 res.json({ message });
             }
         });
+    }
+}
+
+export class APIError extends Error {
+    public status: number;
+
+    constructor(message: string, status = 400) {
+        super(message);
+        this.status = status;
+    }
+}
+
+function getParams(method: MethodLowerCase, req: Request): Record<string, any> {
+    if (method === "get" || method === "delete") {
+        const params: Record<string, any> = {};
+        for (let [key, value] of Object.entries(req.query)) {
+            if (typeof value === "string") {
+                try {
+                    value = JSON.parse(value);
+                } catch (e) { }
+            }
+            params[key] = value;
+        }
+        return params;
+    } else {
+        return req.body;
     }
 }
